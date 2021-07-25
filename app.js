ea27f726c62d4a7e45518ecfc4d0a202f98ebfbd
gunjuzone <Gunjuzone@gmail.com>
2021-07-26 00:52:56 +0100

    Add event listener to the button to select the colors

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+btn.addEventListener('click', function () {
+  // The logic here is to get random number of colors array from 0 -3
+  // console.log(document.body);
+});
